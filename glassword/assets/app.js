d1e90e657515ccffd94b6d839899a2dd797a2178
Snehal Bodile <contact.snehalbodile@gmail.com>
2024-11-20 21:57:25 +0530

    Integration reactjs with symfony

--- glassword/assets/app.js
+++ glassword/assets/app.js
@@ -1,10 +1,18 @@
-import './bootstrap.js';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
-import './styles/app.css';
+// import './bootstrap.js';
+// /*
+//  * Welcome to your app's main JavaScript file!
+//  *
+//  * This file will be included onto the page via the importmap() Twig function,
+//  * which should already be in your base.html.twig.
+//  */
+// import './styles/app.css';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+// console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
